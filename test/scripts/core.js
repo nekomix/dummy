d6a5eaf260ead1b9f7c60a972408af757349d5b4
nekomix <85521501+nekomix@users.noreply.github.com>
2022-07-28 03:02:02 +0800

    Update core.js

--- test/scripts/core.js
+++ test/scripts/core.js
@@ -1,4 +1,4 @@
-var filepath = "https://raw.githubusercontent.com/nekomix/dummy/main/_performance_HKV_compact.csv?1";
+var filepath = "https://raw.githubusercontent.com/nekomix/dummy/main/_performance_HKV_compact.csv?2";
 
 // --------------------------------------------------------
 
